6f2f9b67624ee296eb0b496047219f2ddd24ce93
simple <46109085+simple-official@users.noreply.github.com>
2019-01-30 11:26:55 +0100

    Update sc-config.php
    We've updated the sc-config.php to use simplecontent when you clone the repository.

--- sc-config.php
+++ sc-config.php
@@ -6,12 +6,12 @@
    * 
    * - simplecontent 
    */
-  define('MYSQL_HOST', 'localhost');
-  define('MYSQL_USER', 'root');
-  define('MYSQL_PASS', '');
-  define('MYSQL_NAME', 'a7956');
-  define('TBL_PRF', 'asd098as40d98as40d980as84d_');
-  define('ACTIVATE', 'no_____');
+  define('MYSQL_HOST', 'HERE_MYSQL_HOST');
+  define('MYSQL_USER', 'HERE_MYSQL_USER');
+  define('MYSQL_PASS', 'HERE_MYSQL_PASS');
+  define('MYSQL_NAME', 'HERE_MYSQL_NAME');
+  define('TBL_PRF', 'TABLE_PREFIX_HERE');
+  define('ACTIVATE', 'no___');
   
-  $secure_code = "b60edaef29a9e05e7ab3a075f9d52f6ec6bb5ccc3623c8f5b7c5e54b39a74d17fde600499182b163825c25d4400ef57245145aaa74bf7d543623c8f5b7c5e54b39a74d17fde600499182b163154541530679034";
+  $secure_code = "HERE_SECURE_CODE";
 ?>
